07de3caebcfbf25a1ef88a9f97e0def3e4fa8697
emerson2192 <emersonr19@gmail.com>
2023-09-14 21:49:25 -0300

    subindo pela premera vez

---
+++ pythonProject.py
@@ -0,0 +1,1 @@
+print('oLÁ MUNDO')
---
+++ pythonProject1.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número:  '))
+n2 = int(input('Digite um número:  '))
+soma: int = n1 + n2
+print('A some entre {} e {} é {}'.format(n1, n2, soma))
+
+
+
+
